41064c2ac82f12b6ffbd977f374bf03329aff185
AyahHameedat <aya.hameedat@gmail.com>
2022-06-19 23:33:22 +0300

    setup

--- toysexchange/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ toysexchange/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,5 +5,22 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "auth": {
+        "toysexchange44067892": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s315a1f13e": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }

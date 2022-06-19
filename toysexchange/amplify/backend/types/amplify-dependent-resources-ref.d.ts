d94b62a8ef2080f2a135f69e737ef10b847233f0
AyahHameedat <aya.hameedat@gmail.com>
2022-06-19 12:49:43 +0300

    setup is done

---
+++ toysexchange/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "toysexchange": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}

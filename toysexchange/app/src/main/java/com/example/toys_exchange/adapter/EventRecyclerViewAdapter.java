5bd9687dee980e2ada18a8049883f26575b5e0ec
AlaaYlula <alaamwazani@hotmail.com>
2022-06-23 10:59:53 +0300

    Add Tab

--- toysexchange/app/src/main/java/com/example/toys_exchange/adapter/EventRecyclerViewAdapter.java
+++ toysexchange/app/src/main/java/com/example/toys_exchange/adapter/EventRecyclerViewAdapter.java
@@ -1,4 +1,4 @@
-package com.example.toys_exchange;
+package com.example.toys_exchange.adapter;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.amplifyframework.datastore.generated.model.Event;
+import com.example.toys_exchange.R;
 
 import java.util.List;
 

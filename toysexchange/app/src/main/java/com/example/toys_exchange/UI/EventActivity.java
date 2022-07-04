c6c639b7e4aff4277d240d7d02b9ee7b94baef5e
AlaaYlula <alaamwazani@hotmail.com>
2022-07-04 23:53:50 +0300

    update on App

--- toysexchange/app/src/main/java/com/example/toys_exchange/UI/EventActivity.java
+++ toysexchange/app/src/main/java/com/example/toys_exchange/UI/EventActivity.java
@@ -76,7 +76,6 @@ public class EventActivity extends AppCompatActivity {
         setSupportActionBar(toolBar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Add Event");
-        enableLocation();
 
          eventDescription = findViewById(R.id.etDescription);
          eventTitle = findViewById(R.id.etTitle);
@@ -114,65 +113,7 @@ public class EventActivity extends AppCompatActivity {
         });
         
     }
-    ///////////////////////////////////////////////////// Location //////////////////////////////////////////////
-    @SuppressLint("MissingPermission")
-    private void enableLocation() {
-        // check if permissions are given
-        if (checkPermissions()) {
-            // check if location is enabled
-            if (isLocationEnabled()) {
-                Toast.makeText(this, "Your Location Enabled... ", Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(this, "Please turn on your location...", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(intent);
-            }
-        } else {
-            // if permissions aren't available,
-            // request for permissions
-            requestPermissions();
-        }
-    }
 
-
-    // method to check for permissions
-    private boolean checkPermissions() {
-        return ActivityCompat
-                .checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) ==
-                PackageManager.PERMISSION_GRANTED
-                &&
-                ActivityCompat
-                        .checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
-                        PackageManager.PERMISSION_GRANTED;
-
-    }
-
-    // method to request for permissions
-    private void requestPermissions() {
-        ActivityCompat.requestPermissions(this, new String[]{
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_ID);
-    }
-
-    // method to check
-    // if location is enabled
-    private boolean isLocationEnabled() {
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
-                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-    }
-
-    // If everything is alright then
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode == PERMISSION_ID) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                enableLocation();
-            }
-        }
-    }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
     private void authAttribute(){
         Amplify.Auth.fetchUserAttributes(
@@ -276,9 +217,6 @@ public class EventActivity extends AppCompatActivity {
     @Override
     public void onResume() {
         super.onResume();
-        if (checkPermissions()) {
-            enableLocation();
-        }
 
         Intent locationIntent=getIntent();
         longitude= locationIntent.getDoubleExtra("longitude",0.0);
